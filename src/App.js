e9efb98c04b29bdeac4342ae42801de2aeb13ba4
SUDHAKAR1202 <sudhakargmk12@gmail.com>
2024-11-22 13:11:50 +0530

    updated the app js code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const backendUrl = "http://localhost:5000";
 
+  const setCookie = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/set-cookie`, {
+        withCredentials: true,
+      });
+      alert(response.data.message);
+    } catch(err) {
+      console.error(err);
+    }
+  };
+
+  const getCookie = async (statusCode) => {
+
+    try {
+      const response = await axios.get(`${backendUrl}/status/${statusCode}`);
+      alert(`${response.status}: ${response.data.message}`);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const sendStatusRequest = async (statusCode) => {
+    try {
+      const response = await axios.get(`${backendUrl}/status/${statusCode}`);
+      alert(`${response.status}: ${response.data.message}`);
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Express.js Cookie & Status Code Demo</h1>
+      <button onClick={setCookie}>Set Cookie</button>&nbsp;&nbsp;
+      <button onClick={getCookie}>Get Cookie</button>&nbsp;&nbsp;
+      <button onClick={() => sendStatusRequest(200)}>Send 200</button>&nbsp;&nbsp;
+      <button onClick={() => sendStatusRequest(201)}>Send 201</button>&nbsp;&nbsp;
+      <button onClick={() => sendStatusRequest(400)}>Send 400</button>&nbsp;&nbsp;
+      <button onClick={() => sendStatusRequest(404)}>Send 404</button>&nbsp;&nbsp;
+      <button onClick={() => sendStatusRequest(500)}>Send 500</button>
+    </div>
+  )
+
+}
 export default App;
